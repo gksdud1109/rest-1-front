7e2101db7de30e92e96531f8be636be9c29abe7d
gksdud1109 <vectorh532@gmail.com>
2025-09-12 15:14:23 +0900

    feat page, add post.ts

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useState, useEffect } from "react"; 
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { PostDto } from "@/type/post";
 
 export default function Home() {
 
-  const [posts, setPosts] = useState<{id:number, title:string}[]>([]);
+  const [posts, setPosts] = useState<PostDto[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:8080/api/v1/posts")
@@ -15,13 +17,22 @@ export default function Home() {
   },[]);
 
   return (
-  <div className="flex flex-col gap-9">  
-    <h1>글 목록</h1>
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>{post.id} : {post.title}</li>
-      ))}
-    </ul>
-  </div>
+    
+    <div className="flex flex-col gap-9">  
+      <h1>글 목록</h1>
+      {posts.length===0 && <div>Loading...</div>}
+      {posts.length >0 && (
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+            {post.id} : {post.title}
+            </Link>
+          </li>
+          
+        ))}
+      </ul>
+      )}
+    </div>
   );
 }
