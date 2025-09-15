bef1bab7beebcd41263605d96dc96847b0d6e3ba
gksdud1109 <vectorh532@gmail.com>
2025-09-15 15:42:58 +0900

    work

--- src/app/posts/[id]/edit/page.tsx
+++ src/app/posts/[id]/edit/page.tsx
@@ -1,75 +1,107 @@
 "use client";
 
 import { fetchApi } from "@/lib/client";
-import { PostDto } from "@/type/post";
+import { PostCommentDto, PostDto } from "@/type/post";
+import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const { id } = useParams();
+  const { id: postId } = useParams();
   const router = useRouter();
 
   const [post, setPost] = useState<PostDto | null>(null);
+  const [postComments, setPostComments] = useState<PostCommentDto[] | null>(
+    null
+  );
 
   useEffect(() => {
-    fetchApi(`/api/v1/posts/${id}`).then(setPost);
+    fetchApi(`/api/v1/posts/${postId}`)
+      .then(setPost)
+      .catch((err) => {
+        alert(err);
+        router.replace("/posts");
+      });
+
+    fetchApi(`/api/v1/posts/${postId}/comments`).then(setPostComments);
   }, []);
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-
-    const form = e.target;
-
-    const titleInput = form.title;
-    const contentText = form.content;
-
-    if (titleInput.value.length === 0) {
-      alert("제목을 입력해주세요.");
-      titleInput.focus();
-    }
-
-    if (contentText.value.length === 0) {
-      alert("내용을 입력해주세요.");
-      contentText.focus();
-    }
-
+  const deletePost = (id: number) => {
     fetchApi(`/api/v1/posts/${id}`, {
-      method: "PUT",
-      body: JSON.stringify({
-        title: titleInput.value,
-        content: contentText.value,
-      }),
+      method: "DELETE",
     }).then((data) => {
       alert(data.msg);
-      router.replace(`/posts/${id}`);
+      router.replace("/posts");
+    });
+  };
+
+  const deletePostComment = (commentId: number) => {
+    fetchApi(`/api/v1/posts/${postId}/comments/${commentId}`, {
+      method: "DELETE",
+    }).then((data) => {
+      alert(data.msg);
+
+      if (postComments === null) return;
+
+      // 리렌더링을 위한 댓글 배열 교체 필요
+      setPostComments(
+        postComments.filter((postComment) => postComment.id !== commentId)
+      );
     });
   };
 
   if (post === null) {
     return <div>Loading...</div>;
   }
-
   return (
     <>
-      <h1 className="text-center">글 수정</h1>
-      <form className="flex flex-col gap-2 p-2" onSubmit={handleSubmit}>
-        <input
-          className="border border-gray-300 rounded p-2"
-          type="text"
-          name="title"
-          placeholder="제목"
-          defaultValue={post.title}
-        />
-        <textarea
-          className="border border-gray-300 rounded p-2"
-          name="content"
-          placeholder="내용"
-          defaultValue={post.content}
-        />
-        <button className="bg-blue-500 text-white p-2 rounded" type="submit">
+      <h1 className="p-2">글 상세 보기</h1>
+
+      <div>
+        <div>번호 : {post.id}</div>
+        <div>제목 : {post.title}</div>
+        <div>내용 : {post.content}</div>
+      </div>
+
+      <div className="flex gap-4">
+        <Link className="border-2 p-2 rounded" href={`/posts/${post.id}/edit`}>
           수정
+        </Link>
+        <button
+          className="border-2 p-2 rounded"
+          onClick={() => {
+            deletePost(post.id);
+          }}
+        >
+          삭제
         </button>
-      </form>
+      </div>
+
+      <h2 className="p-2">댓글 목록</h2>
+      {postComments === null && <div>Loading...</div>}
+      {postComments !== null && postComments.length === 0 && (
+        <div>댓글이 없습니다.</div>
+      )}
+
+      {postComments !== null && postComments.length > 0 && (
+        <ul className="flex flex-col gap-2">
+          {postComments.map((postComment) => (
+            <li key={postComment.id} className="flex gap-2 items-center">
+              <span>{postComment.id} : </span>
+              <span>{postComment.content}</span>
+              <button className="border-2 p-2 rounded">수정</button>
+              <button
+                className="border-2 p-2 rounded"
+                onClick={() => {
+                  deletePostComment(postComment.id);
+                }}
+              >
+                삭제
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -6,6 +6,71 @@ import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
+function PostCommentListItem({
+  postComment,
+  deletePostComment,
+  postId,
+  onModifySuccess,
+}: {
+  postComment: PostCommentDto;
+  deletePostComment: (commentId: number) => void;
+  postId: number;
+  onModifySuccess: (id: number, contentValue: string) => void;
+}) {
+  const [modifyMode, setModifyMode] = useState(false);
+
+  const toggleModifyMode = () => {
+    setModifyMode(!modifyMode);
+  };
+
+  const handleModifySubmit = (e: any) => {
+    e.preventDefault();
+    const form = e.target;
+    const contentInput = form.content;
+    const contentValue = contentInput.value;
+
+    fetchApi(`/api/v1/posts/${postId}/comments/${postComment.id}`, {
+      method: "PUT",
+      body: JSON.stringify({ content: contentValue }),
+    }).then((data) => {
+      alert(data.msg);
+      toggleModifyMode();
+      onModifySuccess(postComment.id, contentValue);
+    });
+  };
+
+  return (
+    <li key={postComment.id} className="flex gap-2 items-center">
+      <span>{postComment.id} : </span>
+      {modifyMode && (
+        <form className="flex gap-2" onSubmit={handleModifySubmit}>
+          <input
+            type="text"
+            name="content"
+            defaultValue={postComment.content}
+            className="border-2 p-2 rounded"
+          />
+          <button className="border-2 p-2 rounded" type="submit">
+            저장
+          </button>
+        </form>
+      )}
+      {!modifyMode && <span>{postComment.content}</span>}
+      <button className="border-2 p-2 rounded" onClick={toggleModifyMode}>
+        {modifyMode ? "수정취소" : "수정"}
+      </button>
+      <button
+        className="border-2 p-2 rounded"
+        onClick={() => {
+          deletePostComment(postComment.id);
+        }}
+      >
+        삭제
+      </button>
+    </li>
+  );
+}
+
 export default function Home() {
   const { id: postId } = useParams();
   const router = useRouter();
@@ -16,7 +81,13 @@ export default function Home() {
   );
 
   useEffect(() => {
-    fetchApi(`/api/v1/posts/${postId}`).then(setPost);
+    fetchApi(`/api/v1/posts/${postId}`)
+      .then(setPost)
+      .catch((err) => {
+        alert(err);
+        router.replace("/posts");
+      });
+
     fetchApi(`/api/v1/posts/${postId}/comments`).then(setPostComments);
   }, []);
 
@@ -44,10 +115,21 @@ export default function Home() {
     });
   };
 
+  const onModifySuccess = (id: number, contentValue: string) => {
+    if (postComments === null) return;
+
+    setPostComments(
+      postComments.map((postComment) =>
+        postComment.id === id
+          ? { ...postComment, content: contentValue }
+          : postComment
+      )
+    );
+  };
+
   if (post === null) {
     return <div>Loading...</div>;
   }
-
   return (
     <>
       <h1 className="p-2">글 상세 보기</h1>
@@ -81,19 +163,13 @@ export default function Home() {
       {postComments !== null && postComments.length > 0 && (
         <ul className="flex flex-col gap-2">
           {postComments.map((postComment) => (
-            <li key={postComment.id} className="flex gap-2 items-center">
-              <span>{postComment.id} : </span>
-              <span>{postComment.content}</span>
-              <button className="border-2 p-2 rounded">수정</button>
-              <button
-                className="border-2 p-2 rounded"
-                onClick={() => {
-                  deletePostComment(postComment.id);
-                }}
-              >
-                삭제
-              </button>
-            </li>
+            <PostCommentListItem
+              key={postComment.id}
+              postComment={postComment}
+              deletePostComment={deletePostComment}
+              postId={post.id}
+              onModifySuccess={onModifySuccess}
+            />
           ))}
         </ul>
       )}
